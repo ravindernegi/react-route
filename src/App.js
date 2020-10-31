246f3954db187e86ac7a5ac64b10f259122d561a
Ravinder Negi <r.singh.negi85@gmail.com>
2020-10-31 23:13:09 +0530

    404 and redirect options

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {BrowserRouter as Router,Switch,Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router,Switch,Route, Link, Redirect} from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import NoFound from './404';
 const App = (props) => {
   return (
     <div className="container" >
       <h1 >React Route</h1>
-
-
         <Router>
         <p align="center">
         <Link to="/">Home</Link>
@@ -25,6 +24,11 @@ const App = (props) => {
             <Route exact path="/contact">
               <Contact/>
             </Route>
+            {/*<Route path="*">
+              <NoFound/>
+            </Route>
+  */}
+            <Redirect to="/"/>
         </Switch>
       </Router>
 

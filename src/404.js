e1084256401f7d81cdbf44ce120e8928c81f84ea
Ravinder Negi <r.singh.negi85@gmail.com>
2020-10-31 23:13:22 +0530

    404 and redirect options

---
+++ src/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const NotFound = (props) => {
+  return (
+      <div className="page">
+          <h2>404 Error</h2>
+          <p>Page does not exsit!</p>
+      </div>
+  )
+}
+
+export default NotFound;

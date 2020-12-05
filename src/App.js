146d84c049810d7226fa5d39a6c926ecbbd647b2
Ravinder Negi <r.singh.negi85@gmail.com>
2020-12-05 23:17:34 +0530

    made navlink into react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router,Switch,Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router,Switch,Route, Link,NavLink} from 'react-router-dom';
 import routes from './routes';
 const App = (props) => {
   return (
@@ -7,17 +7,40 @@ const App = (props) => {
       <h1 >React Route</h1>
         <Router>
         <p align="center">
-        <Link to="/">Home</Link>
-        | <Link to="/about">About</Link> |
-         <Link to="/contact">Contact</Link></p>
+          <NavLink exact to="/" activeClassName="active">Home</NavLink>
+        | <NavLink exact to="/about" activeStyle={{
+           color:'red',
+           fontWeight:'bold'
+         }}>About</NavLink> |
+         <NavLink exact to="/contact" activeStyle={{
+           color:'red',
+           fontWeight:'bold'
+         }}>Contact</NavLink>
+
+         | <Link to="/test">Test</Link>
+
+         | <NavLink exact to="/faq" activeStyle={{
+           color:'red',
+           fontWeight:'bold'
+         }}>FAQ</NavLink>
+
+        </p>
 
         <Switch>
           {
             routes.map((route,index)=>(<Route key={index} exact={route.exact} path={route.path} render={()=>(<route.component/>)}/>))
           }
         </Switch>
-      </Router>
 
+      </Router>
+           <style>
+             {
+               `
+               .active{color:#ff0;font-weight:bold;}
+
+               `
+             }
+           </style>
     </div >
   )
 }
